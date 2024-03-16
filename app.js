18eaf8c723578ba1e76b85b8249c36da75588ed4
Daniel Susetio <dsusetio87@gmail.com>
2024-03-16 13:34:14 +0100

    .

--- app.js
+++ app.js
@@ -11,10 +11,6 @@ const express = require("express");
 
 const app = express();
 
-app.use(cors());
-app.use(express.static("public"));
-app.use(express.json());
-
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
 
@@ -35,7 +31,9 @@ app.use("/order", orderRoutes);
 const checkoutRoutes = require("./routes/checkout.routes");
 app.use("/checkout", checkoutRoutes); 
 
+
 // seperate route for guest users
+
 
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
